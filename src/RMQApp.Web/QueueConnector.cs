e850720dacd41ea3e0b053f19d32d4a184c1fd10
Dmitrii Sosedov <dsosedov@pivotal.io>
2020-02-10 14:58:38 -0500

    WIP

--- src/RMQApp.Web/QueueConnector.cs
+++ src/RMQApp.Web/QueueConnector.cs
@@ -1,5 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using RabbitMQ.Client;
+using Steeltoe.CloudFoundry.Connector.RabbitMQ;
 
 namespace RMQApp.Web
 {
@@ -7,15 +9,36 @@ namespace RMQApp.Web
     {
         const string QueueName = "rq";
 
-        private static QueueConnector instance = null;
-        private static readonly object @lock = new object();
+        static QueueConnector instance = null;
+        static readonly object @lock = new object();
 
         readonly IConnection _connection;
         readonly IModel _channel;
 
         QueueConnector()
         {
-            var factory = new ConnectionFactory() { HostName = "192.168.64.29", Port = 31672 };
+            var config = SteeltoeConfigurationManager.Instance.GetConfiguration();
+
+            Console.WriteLine($"Rabbit: {config.GetSection("VCAP_SERVICES").Value}");
+
+            //foreach (var child in config.GetChildren())
+            //{
+            //    Console.WriteLine($"Path: {child.Path}; Key: {child.Key}; Value: {child.Value}");
+            //}
+
+            var options = new RabbitMQProviderConnectorOptions(config.GetSection("VCAP_SERVICES"));
+
+            Console.WriteLine($"Host: {options.Server}");
+
+            var factory = new ConnectionFactory
+            {
+                HostName = "192.168.8.36"/*options.Server*/,
+                Password = "uJeLxtEiYc8U3VzWUQC6K8dy7JjoMhqB"/*options.Password*/,
+                Port = 5672/*options.Port*/,
+                //Uri = new Uri("amqp://df76ef20-a5a5-462a-bdfb-10d1de440841:uJeLxtEiYc8U3VzWUQC6K8dy7JjoMhqB@192.168.8.36:5672/cf087b17-9714-4ec0-b7f9-4ecd6aadd593"/*options.Uri*/),
+                UserName = "df76ef20-a5a5-462a-bdfb-10d1de440841"/*options.Username*/,
+                VirtualHost = "cf087b17-9714-4ec0-b7f9-4ecd6aadd593"/*options.VirtualHost*/,
+            };
 
             _connection = factory.CreateConnection(nameof(RMQApp));
 
---
+++ src/RMQApp.Web/SteeltoeConfigurationManager.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+
+namespace RMQApp.Web
+{
+    public sealed class SteeltoeConfigurationManager
+    {
+        static SteeltoeConfigurationManager instance = null;
+        static readonly object @lock = new object();
+
+        readonly IConfiguration _configuration;
+
+        SteeltoeConfigurationManager()
+        {
+            _configuration = new ConfigurationBuilder()
+                //.AddJsonFile("appsettings.json")
+                //.AddXmlFile()
+                .AddEnvironmentVariables() // bootstrap config-server
+                .AddCloudFoundry()
+                //.AddConfigServer()
+                .AddEnvironmentVariables() // override config-server
+                .Build();
+        }
+
+        ~SteeltoeConfigurationManager()
+        {
+        }
+
+        public IConfiguration GetConfiguration()
+        {
+            return _configuration;
+        }
+
+        public static SteeltoeConfigurationManager Instance
+        {
+            get
+            {
+                lock (@lock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new SteeltoeConfigurationManager();
+                    }
+
+                    return instance;
+                }
+            }
+        }
+    }
+}
